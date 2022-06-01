fd8b832543c00eb6e60ccde77631871f3c22d3fd
ymlai <ymlai@THINKBOOK-14G3A>
2022-06-01 10:43:09 +0800

    Test push

--- app/src/main/java/sg/edu/np/mad/splanner/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/splanner/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.np.mad.splanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView test = findViewById(R.id.test);
+        test.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     }
 }
