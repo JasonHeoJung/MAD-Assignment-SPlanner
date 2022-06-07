72b12c63222a8fd2301104c480fc584c5bfe0fdd
NataliekohNP <s10227870@connect.np.edu.sg>
2022-06-07 22:55:06 +0800

    Layout only for timetable

---
+++ app/src/main/java/sg/edu/np/mad/splanner/timetable_details.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.splanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timetable_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable_details);
+    }
+}
