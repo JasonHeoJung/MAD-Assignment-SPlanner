60a108c466c01547286f14a2fef8016a070922df
ymlai <ymlai@THINKBOOK-14G3A>
2022-06-19 21:24:51 +0800

    minor changes

--- app/src/main/java/sg/edu/np/mad/splanner/HomeFragment.java
+++ app/src/main/java/sg/edu/np/mad/splanner/HomeFragment.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad.splanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -16,6 +18,7 @@ import java.util.Date;
 public class HomeFragment extends Fragment {
 
     private TextView schedule;
+    private Button profile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,10 +26,19 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        profile = view.findViewById(R.id.profile);
         schedule = view.findViewById(R.id.schedule);
         Date dateCurrent = Calendar.getInstance().getTime();
 
         schedule.setText(dateCurrent.toString());
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), UserProfile.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
