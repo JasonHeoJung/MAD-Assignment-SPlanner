c9f9f618e0cad96542031c59903c889dc8775863
jstnde <nahcnitsuj@gmail.com>
2022-06-20 16:49:07 +0800

    add create task

--- app/src/main/java/sg/edu/np/mad/splanner/AddTaskFragment.java
+++ app/src/main/java/sg/edu/np/mad/splanner/AddTaskFragment.java
@@ -2,6 +2,8 @@ package sg.edu.np.mad.splanner;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,48 +12,54 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class AddTaskFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class AddTaskFragment extends Fragment {
+    private FirebaseAuth auth;
+    private DatabaseReference reference;
     MainActivity mainActivity;
-    private Fragment fragment;
     private EditText taskName;
-    private Button addTask;
-    private Button cancelAddTask;
     private EditText date;
-    private Bundle b;
-    private Task t;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_add_task, container, false);
-        mainActivity = (MainActivity) getActivity();
-        addTask = v.findViewById(R.id.addTask);
-        cancelAddTask = v.findViewById(R.id.cancelAddTask);
-        taskName = v.findViewById(R.id.taskName);
-        date = v.findViewById(R.id.dueDate);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_task, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        taskName = view.findViewById(R.id.taskName);
+        date = view.findViewById(R.id.dueDate);
+        Fragment fragment = new ToDoListFragment();
+
+        Button addTask = view.findViewById(R.id.addTask);
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragment = new ToDoListFragment();
                 String name = taskName.getText().toString();
                 String dueDate = date.getText().toString();
-                t = new Task(name, dueDate);
-//                b = new Bundle();
-//                b.putBoolean("status", true);
-//                fragment.setArguments(b);
-                mainActivity.taskList1.setTaskList(t);
+
+                DatabaseReference tasksRef = reference.child(auth.getCurrentUser().getUid()).child("tasks").push();
+                tasksRef.setValue(new Task(name, dueDate));
+
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
             }
         });
+
+        Button cancelAddTask = view.findViewById(R.id.cancelAddTask);
         cancelAddTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragment = new ToDoListFragment();
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
             }
         });
-        // Inflate the layout for this fragment
-        return v;
     }
 }
