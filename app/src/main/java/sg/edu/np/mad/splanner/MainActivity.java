7c3e6d0275f9a240ff5bf2bc0e321fe091ea922b
jstnde <nahcnitsuj@gmail.com>
2022-06-20 17:56:33 +0800

    remove redundant lines

--- app/src/main/java/sg/edu/np/mad/splanner/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/splanner/MainActivity.java
@@ -14,17 +14,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
-    public ArrayList<Task> taskList = new ArrayList<>();
-    public TaskList taskList1;
-    public ArrayList<Score> scoreList = new ArrayList<>();
-    public ScoreList scoreList1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        taskList1 = new TaskList(taskList);
-        scoreList1 = new ScoreList(scoreList);
 
         bottomNavigationView = findViewById(R.id.bottomNav);
 
