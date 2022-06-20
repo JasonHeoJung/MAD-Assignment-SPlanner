4f34a3b09a399db602682f820682172a2882c4a2
jstnde <nahcnitsuj@gmail.com>
2022-06-20 18:38:27 +0800

    add create score

--- app/src/main/java/sg/edu/np/mad/splanner/AddMarksFragment.java
+++ app/src/main/java/sg/edu/np/mad/splanner/AddMarksFragment.java
@@ -2,6 +2,8 @@ package sg.edu.np.mad.splanner;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,44 +12,52 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class AddMarksFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    private Fragment fragment;
-    private Button addSubject;
-    private Button cancelAddSubject;
+public class AddMarksFragment extends Fragment {
+    private FirebaseAuth auth;
+    private DatabaseReference reference;
     private EditText subjectName;
     private EditText grade;
-    private Score s;
-    private MainActivity mainActivity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_add_marks, container, false);
-        mainActivity = (MainActivity) getActivity();
-        addSubject = v.findViewById(R.id.addSubject);
-        cancelAddSubject = v.findViewById(R.id.cancelAddSubject);
-        subjectName = v.findViewById(R.id.subjectName);
-        grade = v.findViewById(R.id.grade);
+        return inflater.inflate(R.layout.fragment_add_marks, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        subjectName = view.findViewById(R.id.subjectName);
+        grade = view.findViewById(R.id.grade);
+        Fragment fragment = new TrackerFragment();
+
+        Button addSubject = view.findViewById(R.id.addSubject);
         addSubject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragment = new TrackerFragment();
                 String subjName = subjectName.getText().toString();
                 String grade1 = grade.getText().toString();
-                s = new Score(subjName, grade1);
-                mainActivity.scoreList1.setScoreList(s);
+
+                DatabaseReference scoresRef = reference.child(auth.getCurrentUser().getUid()).child("scores").push();
+                scoresRef.setValue(new Score(subjName, grade1));
+
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
             }
         });
+
+        Button cancelAddSubject = view.findViewById(R.id.cancelAddSubject);
         cancelAddSubject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragment = new TrackerFragment();
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
             }
         });
-        // Inflate the layout for this fragment
-        return v;
     }
 }
