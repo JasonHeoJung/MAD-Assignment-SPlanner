ce046aa262ac005662d85ca4e41aca5e7a9d1afc
jstnde <nahcnitsuj@gmail.com>
2022-06-20 16:51:32 +0800

    remove unnecessary declaration

--- app/src/main/java/sg/edu/np/mad/splanner/ToDoListViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/splanner/ToDoListViewHolder.java
@@ -9,16 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ToDoListViewHolder extends RecyclerView.ViewHolder {
     TextView taskName;
     TextView dueDate;
-    TextView title;
-    TextView title2;
     Button complete;
 
     public ToDoListViewHolder(View v) {
         super(v);
         taskName = v.findViewById(R.id.Task);
         dueDate = v.findViewById(R.id.DueDate);
-        title = v.findViewById(R.id.textView11);
-        title2 = v.findViewById(R.id.textView12);
         complete = v.findViewById(R.id.status);
     }
 }
