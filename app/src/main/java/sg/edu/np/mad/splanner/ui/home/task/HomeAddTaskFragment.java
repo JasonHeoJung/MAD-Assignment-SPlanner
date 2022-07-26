c087aebdc2f5a313eb0a1edab978e0001f5211be
jstnde <nahcnitsuj@gmail.com>
2022-07-27 03:47:01 +0800

    add home add task frag,, set cancelbtn onclick to home frag

---
+++ app/src/main/java/sg/edu/np/mad/splanner/ui/home/task/HomeAddTaskFragment.java
@@ -0,0 +1,44 @@
+package sg.edu.np.mad.splanner.ui.home.task;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import sg.edu.np.mad.splanner.R;
+import sg.edu.np.mad.splanner.databinding.FragmentHomeAddTaskBinding;
+import sg.edu.np.mad.splanner.ui.home.HomeFragment;
+
+public class HomeAddTaskFragment extends Fragment {
+
+    private FragmentHomeAddTaskBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeAddTaskBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Button cancelBtn = root.findViewById(R.id.cancelAddTask);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment_activity_main, new HomeFragment());
+                transaction.commit();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
