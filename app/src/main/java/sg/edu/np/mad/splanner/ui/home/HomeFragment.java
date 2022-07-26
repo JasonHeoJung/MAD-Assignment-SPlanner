cdf21ab123be1e927637dffe34ffac51c337ee40
jstnde <nahcnitsuj@gmail.com>
2022-07-27 00:34:36 +0800

    add onswipe to change between task & schedule

--- app/src/main/java/sg/edu/np/mad/splanner/ui/home/HomeFragment.java
+++ app/src/main/java/sg/edu/np/mad/splanner/ui/home/HomeFragment.java
@@ -1,7 +1,11 @@
 package sg.edu.np.mad.splanner.ui.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,13 +19,24 @@ import sg.edu.np.mad.splanner.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private int pagerValue = 0;
+    GestureDetector gesture;
 
+    @SuppressLint("ClickableViewAccessibility")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.HomeFrame, new HomeTaskFragment()).commit();
+
+        setGesture();
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gesture.onTouchEvent(event);
+            }
+        });
 
         return root;
     }
@@ -30,5 +45,56 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void setGesture() {
+        gesture = new GestureDetector(getActivity(),
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onDown(MotionEvent e) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                        final int SWIPE_MIN_DISTANCE = 120;
+                        final int SWIPE_MAX_OFF_PATH = 250;
+                        final int SWIPE_THRESHOLD_VELOCITY = 200;
+                        try {
+                            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                                return false;
+                            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+                                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                                swipeLeft();
+                            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                                swipeRight();
+                            }
+                        } catch (Exception e) {
+                            // nothing
+                        }
+                        return super.onFling(e1, e2, velocityX, velocityY);
+                    }
+                });
+    }
+
+    private void swipeLeft() {
+        Log.v("SWIPE TEST", "Right to Left");
+
+        if (pagerValue == 0) {
+            FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.HomeFrame, new HomeScheduleFragment()).commit();
+            pagerValue = 1;
+        }
+    }
+
+    private void swipeRight() {
+        Log.v("SWIPE TEST", "Left to Right");
+
+        if (pagerValue == 1) {
+            FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.HomeFrame, new HomeTaskFragment()).commit();
+            pagerValue = 0;
+        }
     }
 }
